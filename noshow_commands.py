3e6ed9c866abb74cc7ef9caf2a23ae9708755cad
so.okada@gmail.com <so-okada>
2020-08-09 12:50:55 +0900

    update

--- noshow_commands.py
+++ noshow_commands.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # a simple analyzer for attendance reports of Microsoft 365 meetings.
 # Version 0.0.1, initial release at 2020-07-05
-# https://so-okada.github.io/noshow/
+# https://github.com/so-okada/noshow/
 # Written by So.Okada, so.okada@gmail.com
 
 import re
